4ebb8dc6d2610aef967d86ba3097123ca2ede705
Pedro <pedrinho.top07@gmail.com>
2021-05-13 20:57:44 -0300

    Adicionando o primeiro projeto de App Mobile no github

---
+++ AndroidProject/app/src/main/java/com/example/exercicio1_firstproject/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.exercicio1_firstproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortearNum(View view){
+
+
+        TextView texto = findViewById(R.id.txtNum);
+
+        int number = new Random().nextInt(11);
+
+
+        texto.setText ("Número: "+number);
+
+
+    }
+
+}
